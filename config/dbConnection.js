44605d93b201d96962770422bfcecd8f52940614
nabraj <nabraj.chaudhary@hazesoft.co>
2024-03-09 19:51:33 +0545

    fix: new changes

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -7,7 +7,7 @@ const createConnection = () => {
   const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'testuser',
+    password: 'password',
     database: 'ddac_repo',
   });
 
