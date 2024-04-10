157e6becdc04d08c63f73592ff34489029a5b163
nabraj <nabraj.chaudhary@hazesoft.co>
2024-04-10 22:12:43 +0545

    fix: new

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -6,13 +6,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 // Function to create and return a connection object
 
-const user = process.env.USER || '';
-const host = process.env.HOST || '';
-const password = process.env.PASSWORD || '';
-const database = process.env.DATABASE || '';
+// const user = process.env.USER || '';
+// const host = process.env.HOST || '';
+// const password = process.env.PASSWORD || '';
+// const database = process.env.DATABASE || '';
 
-const createConnection = () => {
-  const connection = mysql.createConnection({
+const user = 'root';
+const host = 'ddac-database.cluster-cp8go66mcog5.us-east-1.rds.amazonaws.com';
+const password = '#*5NsYL[P~[rNkWKw$W7~T?$jjhA';
+const database = 'ddac_app';
+
+const createConnection = async () => {
+  const connection = await mysql.createConnection({
     host: host,
     user: user,
     password: password,
